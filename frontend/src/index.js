3a2e52dd4ab48c6987b5f527c676511ff73f8812
Prakhar Buliya <76914126+PrakharBuliya@users.noreply.github.com>
2023-04-07 21:09:25 +0000

    Added themeprovider in Index.js file

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -6,13 +6,17 @@ import { store } from "./store";
 
 import App from "./components/App";
 import { BrowserRouter } from "react-router-dom";
+import theme from "./styles/theme";
 
 ReactDOM.render(
+  <ThemeProvider theme={theme}>
   <Provider store={store}>
     <BrowserRouter basename="/">
       <App />
     </BrowserRouter>
-  </Provider>,
+    <theme />
+  </Provider>
+  </ThemeProvider>,
 
   document.getElementById("root")
 );
